311b1dd80e22a66fc70f08d3e4b373217f6381fd
dylan_DPC <dylan.dpc@gmail.com>
2018-11-29 17:12:02 +0530

    now with shiny docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,39 @@
-#![feature(try_from)]
+//! This crate is a clone of the linux [crypt](http://man7.org/linux/man-pages/man3/crypt.3.html) command.
+//! It is a wrapper for the crypto algorithms used in crypt - Md5, blowfish, SHA256 and SHA512.
+//! The algorithm is chosen based on the first 3 characters of the secret. If it starts with `$x$` where x is one of
+//! 1, 2, 3, 5 or 6.
+//!
+//! Till this release (0.1), only MD5($1$), SHA256 ($5$) and SHA512 ($6$) are supported.
+//!
+//! # SECURITY ALERT:
+//! The package is provided for the purposes of interoperability with protocols and systems that mandate the use of MD5.
+//! However, MD5 should be considered cryptographically broken and unsuitable for further use.
+//! Collision attacks against MD5 are both practical and trivial, and theoretical attacks against MD5 have been found.
+//! RFC6151 advises no new protocols to be designed with any MD5-based constructions, including HMAC-MD5.
+//!
+//!
+//! # INSTALLATION:
+//! To add crypt to your package, add this to your Cargo.toml:
+//!
+//! ```toml
+//! [dependencies]
+//! crypt3 = "0.1"
+//! ```
+//!
+//! # EXAMPLES:
+//!
+//! ```rust
+//! use crypt3::crypt;
+//!
+//! let digest = crypt(b"abcdefghijklmnop", b"$1$");
+//! assert_eq!(digest.unwrap(), [36, 49, 36, 36, 29, 100, 220, 226, 57, 196, 67, 123, 119, 54, 4, 29, 176, 137, 225, 185]);
+//! ```
+//!
 
-use md5::{compute, Digest};
-use std::{convert::TryInto, error::Error, fmt::Debug, str};
-use ring::digest::{digest, SHA256, SHA512, Digest as RingDigest};
+use md5::compute;
+use md5::Digest;
+use ring::digest::{digest, Digest as RingDigest, SHA256, SHA512};
+use std::{error::Error, str};
 
 const FORMATS: &[(&str, &str); 6] = &[
     ("md5", "$1$"),
@@ -13,42 +44,58 @@ const FORMATS: &[(&str, &str); 6] = &[
     ("des", ""),
 ];
 
-pub fn crypt(password: &mut [u8], salt: &mut [u8]) -> Result<Encrypted, Box<Error>> {
+/// This function is the entry point of the crate. It accepts 2 byte slices - a to-be-hashed
+/// password and a salt. The first 3 bytes of the salt decide which algorithm is picked and
+/// should be in the $id$ form (e.g. $1$ for MD5).
+
+pub fn crypt<'a>(password: &'a [u8], salt: &'a [u8]) -> Result<Vec<u8>, Box<Error>> {
     if let Some(magic) = format_from_magic(salt) {
-        delegate(magic.0, password, salt)
+        delegate(*magic, password, salt)
     } else {
         Err("cant find algorithm".into())
     }
 }
 
-fn format_from_magic(salt: &mut [u8]) -> Option<&'static (&'static str, &'static str)> {
+fn format_from_magic(salt: &[u8]) -> Option<&'static (&'static str, &'static str)> {
     FORMATS
         .iter()
-        .find(|format| match str::from_utf8(salt.as_ref()) {
-            Ok(s) => s.contains(format.1),
-            _ => false,
-        })
+        .find(|format| salt.starts_with(&format.1.as_bytes()))
 }
 
-fn delegate(
-    algorithm: &str,
-    password: &mut [u8],
-    salt: &mut [u8],
-) -> Result<Encrypted, Box<Error>> {
-    match algorithm {
+fn delegate<'a>(
+    algorithm: (&'a str, &'a str),
+    password: &'a [u8],
+    salt: &'a [u8],
+) -> Result<Vec<u8>, Box<Error>> {
+    let digest: Result<Encrypted, Box<Error>> = match algorithm.0 {
         "md5" => Ok(Encrypted::Md5(compute(password))),
         "sha256" => Ok(Encrypted::Sha256(digest(&SHA256, password))),
         "sha512" => Ok(Encrypted::Sha512(digest(&SHA512, password))),
-        _ => unreachable!()
-        }
-    }
+        _ => Err("algorithm is not supported".into()),
+    };
 
+    Ok(digest?.fill(salt))
+}
 
 #[derive(Clone, Debug)]
-pub enum Encrypted {
+enum Encrypted {
     Md5(Digest),
     Sha256(RingDigest),
     Sha512(RingDigest),
+}
+
+impl Encrypted {
+    pub fn fill<'a>(&'a self, salt: &'a [u8]) -> Vec<u8> {
+        let mut output = salt.to_vec();
+        output.push(b'$');
+        let digest = match *self {
+            Encrypted::Md5(digest) => digest.to_vec(),
+            Encrypted::Sha256(digest) | Encrypted::Sha512(digest) => digest.as_ref().to_vec(),
+        };
+        output.extend(digest.iter());
+
+        output
+    }
 }
 
 #[cfg(test)]
@@ -56,72 +103,49 @@ mod tests {
     use super::*;
     #[test]
     fn format_from_magic_returns_md5() {
-        unsafe {
-            let x = format_from_magic(String::from("$1$").as_bytes_mut());
-            assert_eq!("md5", x.unwrap().0);
-        }
+        let x = format_from_magic(String::from("$1$").as_bytes());
+        assert_eq!("md5", x.unwrap().0);
     }
 
     #[test]
     fn md5_works() {
-        unsafe {
-            let mut password = String::from("abcdefghijklmnop");
-            let mut salt = String::from("$1$");
-
-            let digest = crypt(password.as_bytes_mut(), salt.as_bytes_mut());
-            let expected = compute("abcdefghijklmnop");
-
-            let value = match digest.unwrap() {
-                Encrypted::Md5(x) => x,
-                _ => unreachable!(),
-            };
-
-            assert_eq!(value, expected);
-        }
+        let password = b"abcdefghijklmnop";
+        let salt = b"$1$";
+        let digest = crypt(password, salt).unwrap();
+        let md5 = compute("abcdefghijklmnop");
+        let mut expected = salt.to_vec();
+        expected.push(b'$');
+        expected.extend(md5.to_vec().iter());
+        println!("{:?}", &expected);
+        assert_eq!(digest, expected);
     }
 
     #[test]
     fn sha256_works() {
         use ring::digest;
-        unsafe {
-            let mut password = String::from("309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f
-        989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76fc");
 
-            let mut salt = String::from("$5$");
-
-            let digest = crypt(password.as_bytes_mut(), salt.as_bytes_mut());
-
-            let expected = digest::digest(&digest::SHA256, &password.as_bytes());
-
-            let value = match digest.unwrap() {
-                Encrypted::Sha256(x) => x,
-                _ => unreachable!(),
-            };
-
-            assert_eq!(value.as_ref(), expected.as_ref());
-        }
-
-    }  #[test]
-    fn sha512_works() {
-        use ring::digest;
-        unsafe {
-            let mut password = String::from("309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f
-        989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76fc");
-
-            let mut salt = String::from("$6$");
-
-            let digest = crypt(password.as_bytes_mut(), salt.as_bytes_mut());
-
-            let expected = digest::digest(&digest::SHA512, &password.as_bytes());
-
-            let value = match digest.unwrap() {
-                Encrypted::Sha512(x) => x,
-                _ => unreachable!(),
-            };
-
-            assert_eq!(value.as_ref(), expected.as_ref());
-        }
-
+        let password = b"309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f
+        989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76fc";
+        let salt = b"$5$";
+        let digest = crypt(password, salt).unwrap();
+        let sha256 = digest::digest(&digest::SHA256, &password[..]);
+        let mut expected = salt.to_vec();
+        expected.push(b'$');
+        expected.extend_from_slice(sha256.as_ref());
+        assert_eq!(digest, expected);
     }
 
+    #[test]
+    fn sha512_works() {
+        use ring::digest;
+        let password = b"309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f
+        989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76fc";
+        let salt = b"$6$";
+        let digest = crypt(password, salt).unwrap();
+        let sha512 = digest::digest(&digest::SHA512, &password[..]);
+        let mut expected = salt.to_vec();
+        expected.push(b'$');
+        expected.extend_from_slice(sha512.as_ref());
+        assert_eq!(digest, expected);
+    }
 }
